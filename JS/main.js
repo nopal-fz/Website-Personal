4280a570872bf4d5af1a43df4c7d9ad937e4a5dc
Nopal-Fz <145373069+nopal-fz@users.noreply.github.com>
2024-04-07 10:47:10 +0700

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
 // typed js
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Data Scientist', 'Data Engineer', 'Machine Learning Engineer'],
+    strings: ['Data Scientist', 'Data Engineer', 'Data Analyst', 'Machine Learning Engineer'],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay: 1000,
